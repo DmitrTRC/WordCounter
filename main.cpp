fd1370a099d44fa4fcc63ad8927adf4fca7956bb
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2022-08-04 17:23:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <map>
 
 
+
 //TODO: Open the file from Argument and read word by word.
 // Create dictionary of words and their frequency.
 
@@ -16,32 +17,42 @@
 //to   :  30
 //a    :  20
 
-std::map<std::string,int>dictionary;
+std::map<std::string, int> dictionary;
+std::vector<std::pair<int,std::string>>dictionary2;
 int main (int argc, char **argv) {// Get the file name from the command line.
-    std::fstream file;
+    std::ifstream file;
     std::string word, filename;
-    for (int i=0;i<argc;i++){
-        filename=argv[i];
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
+        return 1;
     }
 
-    file.open(filename);
-    while (file >> word)
-    {
-        if (dictionary.find(word) == dictionary.end())
-            dictionary.insert(make_pair(word, 1));
+    filename = argv[1];
 
-        else
-            dictionary[word]++;
+    file.open (filename);
+    if (!file.is_open ()) {
+        std::cout << "Could not open file " << filename << std::endl;
+        return 1;
     }
 
-    for (auto& i : dictionary) {
-        std::cout << i.first << " - "
-             << i.second<< std::endl;
+    while (file >> word) {
+        dictionary[word]++;
+
     }
+
+    for (auto &[word_, frequency]: dictionary) {
+        dictionary2.emplace_back (frequency, word_);
+    }
+    std::sort (dictionary2.begin (), dictionary2.end ());
+    for (auto item:dictionary2){
+        std::cout<<item.second<<": "<<item.first;
+    }
+    
+
     return 0;
 }
 
-
+//TODO: Output with sorting the dictionary by frequency. ( Highest to lowest )
 
 
 
