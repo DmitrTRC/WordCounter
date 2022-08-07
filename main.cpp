ba9e1c46231abee90669b1c1d1f2eeff92179c72
Dev Spain <9984398@gmail.com>
2022-08-07 15:38:37 +0300

    Ready for PR

--- main.cpp
+++ main.cpp
@@ -50,24 +50,9 @@ int main (int argc, char **argv) {// Get the file name from the command line.
 
     }
 
-    filename = argv[1];
 
-    file.open (filename);
-    if (!file.is_open ()) {
-        std::cout << "Could not open file " << filename << std::endl;
-        return 1;
-    }
-
-    while (file >> word) {
-        dictionary[word]++;
-    }
-
-    for (auto &[word_, frequency]: dictionary) {
-        std::cout << word_ << " - "
-                  << frequency << std::endl;
-
-    }
     std::sort (dictionary2.begin (), dictionary2.end ());
+
     for (auto item:dictionary2){
         std::cout<<item.second<<": "<<item.first;
     }
